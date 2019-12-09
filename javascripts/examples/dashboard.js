88463ae479ebd87c0a468a8efbe8e0922dca2785
rouxiaoxiao <rouxiaoxiao222@gmail.com>
2019-12-09 15:55:56 +0800

    针对要求，修改导航栏，首页不定期检测等内容

--- javascripts/examples/dashboard.js
+++ javascripts/examples/dashboard.js
@@ -128,4 +128,25 @@ $(function () {
         }
     });
 
+//    报警信息提示
+    Command: toastr["error"]("气象站数据出现异常，请及时查看")
+
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "rtl": false,
+        "positionClass": "toast-bottom-right",
+        "onclick": null,
+        "showDuration": 300,
+        "hideDuration": 1000,
+        "timeOut": 5000,
+        "extendedTimeOut": 1000,
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
 });
